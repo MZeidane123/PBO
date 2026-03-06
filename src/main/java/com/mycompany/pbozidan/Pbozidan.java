1b9bad45341c9619ee1a8a54f90398411fb862c1
MZeidane123 <mochzeidane@gmail.com>
2026-03-06 16:40:39 +0700

    Update Pbozidan.java

--- src/main/java/com/mycompany/pbozidan/Pbozidan.java
+++ src/main/java/com/mycompany/pbozidan/Pbozidan.java
@@ -12,25 +12,25 @@ public class Pbozidan {
     public static void main(String[] args) {
         System.out.println("=== SISTEM BANK SEDERHANA ===");
         
-        // Membuat object Customer
+     
         Customer cust1 = new Customer("C001", "Budi Santoso", "081234567890");
-        Customer cust2 = new Customer("C002", "Siti Aminah", "123"); // Akan memicu error validasi no telp
+        Customer cust2 = new Customer("C002", "Siti Aminah", "123"); 
         
-        // Membuat object BankAccount
+
         BankAccount acc1 = new BankAccount("11223344", cust1, 500000);
-        BankAccount acc2 = new BankAccount("99887766", cust2, -100000); // Akan memicu error validasi saldo awal
+        BankAccount acc2 = new BankAccount("99887766", cust2, -100000); 
         
-        // Cek info awal
+ 
         acc1.printAccountInfo();
         
-        // Uji coba metode dengan validasi
-        acc1.deposit(150000);    // Berhasil
-        acc1.deposit(-50000);    // Gagal (Validasi 3)
+   
+        acc1.deposit(150000);    
+        acc1.deposit(-50000);    
         
-        acc1.withdraw(800000);   // Gagal (Validasi 4 - saldo kurang)
-        acc1.withdraw(200000);   // Berhasil
+        acc1.withdraw(800000);  
+        acc1.withdraw(200000);  
         
-        // Cek info akhir
+        
         acc1.printAccountInfo();
     }
 }
