b53edf3275b530f00aaeb02bc2b15168c7b9e751
MZeidane123 <mochzeidane@gmail.com>
2026-03-06 16:40:03 +0700

    Enhance phone number validation in Customer class
    Added validation logic for phone number in setter.

--- src/main/java/com/mycompany/pbozidan/Customer.java
+++ src/main/java/com/mycompany/pbozidan/Customer.java
@@ -10,25 +10,25 @@ package com.mycompany.pbozidan;
  */
 public class Customer {
     
-    // Memenuhi syarat atribut private
+
     private String customerId;
     private String name;
     private String phoneNumber;
 
-    // Constructor ke-1
+  
     public Customer(String customerId, String name, String phoneNumber) {
         this.customerId = customerId;
         this.name = name;
-        setPhoneNumber(phoneNumber); // Menggunakan setter yang memiliki validasi
+        setPhoneNumber(phoneNumber);
     }
 
-    // Getter & Setter dengan access modifier public
+   
     public String getName() { 
         return name; 
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        // Validation Logic 1: Nomor telepon harus valid (minimal 10 digit)
+       
         if (phoneNumber != null && phoneNumber.length() >= 10) {
             this.phoneNumber = phoneNumber;
         } else {
